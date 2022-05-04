612402cf772fb47bbae6e81e1c465ed1df61f32a
Tammy <tammy@deepgenomics.com>
2022-05-04 11:36:58 -0400

    feat: refactoring cache; adding remote url download

--- dnadoh/cache.py
+++ dnadoh/cache.py
@@ -15,7 +15,7 @@ Things to change:
 1.  Download from URLs using the `requests` library.
 2.  Make `FileCache` configurable so that a caller can specify
     a base URL for remote files and the local cache directory
-    (but only has to specify them once).
+    (but only has to specify them once). - done
 3.  Make `FileCache` able to download both remote files and
     local files.
 4.  Check that files whose names are in the cache are actually
@@ -28,8 +28,11 @@ Things to change:
     `staticmethod`.  # because depends on the cache path?
 """
 
+import filecmp
 import requests
 import shutil
+import validators
+
 from pathlib import Path
 from urllib.parse import urljoin
 
@@ -54,32 +57,57 @@ class FileCache:
         else:
             self.remote_url = CONFIG["remote"]
         
-    # Map remote filenames to local cached filenames.
-    _cache = {}
-
-
-    @classmethod
-    def get(cls, filename):
+        self.files = {}
+        
+    def get(self, filename):
         """Return path to cached copy of file, getting as needed."""
-        if filename not in cls._cache:
-            cache_path = Path(CONFIG["cache"], filename)
-            _get_file(filename, cache_path)
-            cls._cache[filename] = cache_path
-        return cls._cache[filename]
+        if filename not in self.files:
+            cache_path = Path(self.cache_dir, filename)
+            # check if valid URL, which implies it can be downloaded
+            if validators.url(filename) is True:
+                if self._download_file(filename, cache_path):
+                    self.files[filename] = cache_path
+                else:
+                    # download failed
+                    print(f"{filename} could not be downloaded.")
+            else:
+                self._get_file(filename, cache_path)
+                self.files[filename] = cache_path
+        else:
+            # 4 - do a further check that the file is actually the same
+            if filecmp.cmp(Path(self.cache_dir, filename), Path(self.remote_url, filename)) is True:  # this doesn't work for remote URLs
+                return self.files[filename]
+            else:
+                self._get_file(filename, cache_path)
+                self.files[filename] = cache_path
 
-    @classmethod
-    def clear(cls):
+    def clear(self):
         """Clear the cache, deleting local files."""
-        cls._cache.clear()
-        for filename in CONFIG["cache"].iterdir():
+        self.files.clear()
+        for filename in self.cache_dir.iterdir():
             filename.unlink()
 
+    def _get_file(self, remote_path, local_path):
+        """Simulate getting a remote file."""
+        remote_path = Path(self.remote_url, remote_path)
+        # print(remote_path)
+        assert remote_path.exists(), f"{remote_path} does not exist"
+        shutil.copyfile(remote_path, local_path)
 
-
-def _get_file(remote_path, local_path):
-    """Simulate getting a remote file."""
-    # TODO; how to check if a remote URL
-    remote_path = Path(CONFIG["remote"], remote_path)
-    # print(remote_path)
-    assert remote_path.exists(), f"{remote_path} does not exist"
-    shutil.copyfile(remote_path, local_path)
+    def _download_file(self, remote_url, local_path):
+        """Downloads a file from a remote url"""
+        # restrict download size - set 250 MB limit
+        h = requests.head(remote_url, allow_redirects=True)
+        header = h.headers
+        content_length = header.get("content-length", None)
+        if content_length > 2.5e-8:  # this is in bytes
+            print(f"File is too large")
+            return False
+        # TODO: file type restrictions?
+        r = requests.get(remote_url, allow_redirects=True)
+        if r.status_code == 200:
+            with open(local_path, "wb") as fhandle:
+                fhandle.write(r.content)
+            return True
+        else:
+            return False
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -4,8 +4,21 @@ import tempfile
 import os
 
 from pathlib import Path
+from unittest import mock
 
-from dnadoh.cache import FileCache, CONFIG
+from dnadoh.cache import FileCache
+
+def _create_file_in_remote(test_fname, cacher):
+    target_path = Path(cacher.cache_dir, test_fname)
+    source_path = Path(cacher.remote_url, test_fname)
+    # remove the file if exists 
+    if os.path.exists(target_path):
+        os.remove(target_path)
+    # create file in remote dir 
+    with open(source_path, "w") as fhandle:
+        fhandle.write("Hello World!")
+    return source_path, target_path
+
 
 @pytest.fixture(scope="module")
 def cacher():
@@ -18,13 +31,20 @@ def cacher():
 
     return cacher
 
-# TODO: how to automatically do this after each test?
-def _clear_cache(cache_dir):
-    """Clears the cache"""
-    for x in os.listdir(cache_dir):
-        full_path = os.path.join(cache_dir, x)
-        if os.path.isfile(full_path):
-            os.remove(full_path)
+
+@pytest.fixture(scope="module")
+def non_empty_cacher():
+    cacher = FileCache()    
+
+    # set up default directories
+    for dir in [cacher.cache_dir, cacher.remote_url]:
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+    
+    test_fname = "test.txt"
+    source_path, target_path = _create_file_in_remote(test_fname, cacher)
+
+    return cacher
 
 
 def test_init():
@@ -39,16 +59,20 @@ def test_init():
 def test_get_when_not_in_cache(cacher):
     """Tests that when a file is not in the cache, that it is fetched"""
     test_fname = "test.txt"
-    target_path = Path(cacher.cache_dir, test_fname)
-    source_path = Path(cacher.remote_url, test_fname)
-    # remove the file if exists 
-    if os.path.exists(target_path):
-        os.remove(target_path)
-    # create file in remote dir 
-    with open(source_path, "w") as fhandle:
-        fhandle.write("Hello World!")
+    source_path, target_path = _create_file_in_remote(test_fname, cacher)
     cacher.get(test_fname)
     assert filecmp.cmp(source_path, target_path) is True
-    _clear_cache(cacher.cache_dir)
+    assert cacher.files[test_fname] == Path(target_path)
+    cacher.clear()
+
+
+@mock.patch.object(FileCache, "_get_file")
+def test_get_when_cached(mock, non_empty_cacher):
+    """Tests that when a file is cached, it is not fetched using _get_file()"""
+    test_fname = "test.txt"
+    non_empty_cacher.get(test_fname)
+    assert mock.call_count == 1
+
+
 
 
