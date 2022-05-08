2e979559469ee765bc273c33ad42e72d930218a2
Tammy <tammy@deepgenomics.com>
2022-05-08 14:53:47 -0400

    update tests

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,83 +1,66 @@
-import filecmp
+import csv
 import pytest
-import tempfile
 import os
+import pyfakefs
 
 from pathlib import Path
 from unittest import mock
 
-from dnadoh.cache import FileCache
-
-def _create_file_in_remote(test_fname, cacher):
-    target_path = Path(cacher.cache_dir, test_fname)
-    source_path = Path(cacher.remote_url, test_fname)
-    # remove the file if exists 
-    if os.path.exists(target_path):
-        os.remove(target_path)
-    # create file in remote dir 
-    with open(source_path, "w") as fhandle:
-        fhandle.write("Hello World!")
-    return source_path, target_path
+from dnadoh.cache import FileCache, CACHE_INDEX, CONFIG, CACHE_HEADER
 
 
-@pytest.fixture(scope="module")
-def cacher():
+@pytest.fixture()
+def cache(fs):
+    for path in CONFIG.values():
+        fs.create_dir(path)
     cacher = FileCache()    
-
-    # set up default directories
-    for dir in [cacher.cache_dir, cacher.remote_url]:
-        if not os.path.exists(dir):
-            os.mkdir(dir)
-
     return cacher
 
 
-@pytest.fixture(scope="module")
-def non_empty_cacher():
-    cacher = FileCache()    
+@pytest.fixture()
+def non_empty_cache(fs):
+    for path in CONFIG.values():
+        fs.create_dir(path)
 
-    # set up default directories
-    for dir in [cacher.cache_dir, cacher.remote_url]:
-        if not os.path.exists(dir):
-            os.mkdir(dir)
+    cache = FileCache()    
     
-    test_fname = "test.txt"
-    source_path, target_path = _create_file_in_remote(test_fname, cacher)
+    test_fname = "foo.txt"
+    fs.create_file(Path(cache.cache_dir, test_fname))
 
-    return cacher
+    return cache
 
-
-def test_init():
+def test_init(fs):
     """Tests that the cache and remote directories are set properly"""
     cache_dir = "/some/test/dir"
+    fs.create_dir(cache_dir)
     remote = "https://www.github.com"
     cache = FileCache(cache_dir, remote)
     assert cache.cache_dir == cache_dir
     assert cache.remote_url == remote
+    assert Path(cache_dir, CACHE_INDEX).exists()
 
 
-def test_get_when_not_in_cache(cacher):
+def test_get_when_not_in_cache(fs, cache):
     """Tests that when a file is not in the cache, that it is fetched"""
     test_fname = "test.txt"
-    source_path, target_path = _create_file_in_remote(test_fname, cacher)
-    cacher.get(test_fname)
-    assert filecmp.cmp(source_path, target_path) is True
-    assert cacher.files[test_fname] == Path(target_path)
-    cacher.clear()
+    fs.create_file(Path(cache.remote_url, test_fname))
+    cache.get(test_fname)
+    assert Path(cache.cache_dir, test_fname).exists()
 
 
 @mock.patch.object(FileCache, "_get_file")
-def test_get_when_cached(mock, non_empty_cacher):
+def test_get_when_cached(mock, non_empty_cache):
     """Tests that when a file is cached, it is not fetched using _get_file()"""
     test_fname = "test.txt"
-    non_empty_cacher.get(test_fname)
+    non_empty_cache.get(test_fname)
     assert mock.call_count == 1
 
-
-def test_remote_download(cacher):
+# not sure why this is failing now
+# OSError: Could not find a suitable TLS CA certificate bundle, invalid path: /Users/tammylau/conda/envs/dnadoh_dev/lib/python3.8/site-packages/certifi/cacert.pem
+def test_remote_download(cache):
     remote_file = "https://github.com/tammy-dg/tammys-simple-web-server/blob/master/test/test.png"
-    cacher.get(remote_file)
-    assert cacher.files[remote_file] == Path(cacher.cache_dir, "test.png")
-    cacher.clear()
+    cache.get(remote_file)
+    print(cache.cache_index)
+    cache.clear()
 
 
