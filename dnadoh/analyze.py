a56b93f0af07da6ba0faec535f6609d1ac1b3138
Tammy <tammy@deepgenomics.com>
2022-06-16 08:19:37 -0400

    feat: using dash for visualization

--- dnadoh/analyze.py
+++ dnadoh/analyze.py
@@ -3,10 +3,15 @@ import argparse
 import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
+import plotly.graph_objects as go
 
 from statannot import add_stat_annotation
+from dash import Dash, dcc, html, Input, Output
 
 from assemble import assemble
+from plotting_utils import add_pvalue_annotation
+
+BASES = ["A", "C", "G", "T"]
 
 # https://github.com/webermarcolivier/statannot/blob/master/example/example.ipynb
 
@@ -51,23 +56,64 @@ def plot_boxplot(df: pd.DataFrame, y_variable: str, location: int) -> None:
     concat_df = pd.concat([subset_df, data], axis=0).sort_values(by=["pid"])
     
     # plot
-    fig = px.box(concat_df, y=y_variable, x="alternate", color="alternate")
-    fig.show()
+    # fig = px.box(concat_df, y=y_variable, x="alternate", color="alternate")
+    fig = go.Figure()
+    bases = ["reference"] + [x for x in BASES if x != reference]
+    for x in bases:
+        fig.add_trace(go.Box(
+            y=concat_df[concat_df['alternate'] == x][y_variable],
+            name=x,
+            boxpoints='outliers'
+        ))
+    fig.title
+    # add_pvalue_annotation(concat_df, fig, "C", [0.8, 0.82])
+
+    return fig
 
 
 
 def main():
     options = parse_args()
-    print('test!')
     assembled_df = assemble(options.input_stem)
 
     locations = get_unique_locations(assembled_df)
 
-    print(assembled_df)
-    print(locations)
+    # print(assembled_df)
+    # print(locations)
 
-    for location in locations:
-        plot_boxplot(assembled_df, "weight", location)
+    app = Dash(__name__)
+
+    app.layout = html.Div([
+        html.Div([
+            html.Div([
+                dcc.Dropdown(
+                    options=sorted(x for x in assembled_df["location"].unique()),
+                    # label='Team',
+                    id='variant-select',
+                    value=locations[0],  # arbitrarily show the first position
+                ),
+            ], style={'width': '50%'})
+        ]),
+
+        dcc.Graph(id='variant'),
+    ])
+
+    @app.callback(
+        Output("variant", "figure"),
+        Input("variant-select", "value")
+    )
+    def update_graph(location):
+        dff = assembled_df[assembled_df["location"] == location]
+        fig = plot_boxplot(assembled_df, "weight", location)
+        return fig
+        
+
+
+
+    # for location in locations:
+    #     plot_boxplot(assembled_df, "weight", location)
+    
+    app.run_server(debug=True)
 
 # test_results = add_stat_annotation(
 #     ax,
@@ -84,11 +130,13 @@ def main():
 
 # plt.show()
 
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     # nothing here for now
     parser.add_argument(
-        "--input_stem", type=str, default=None, help="Path/file stem of synthesized data"
+        "--input_stem", type=str, default=None, help="Path/file stem of synthesized data", required=True
     )
     options = parser.parse_args()
     return options
