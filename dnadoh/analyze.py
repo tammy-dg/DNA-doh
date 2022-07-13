1abc63591dcd593afd3524024c9a137126b10e99
Tammy <tammy@deepgenomics.com>
2022-07-13 10:31:38 -0400

    tinker with aesthetics in plot

--- dnadoh/analyze.py
+++ dnadoh/analyze.py
@@ -16,12 +16,12 @@ from plotting_utils import add_pvalue_annotation
 
 BASES = ["A", "C", "G", "T"]
 
-# https://github.com/webermarcolivier/statannot/blob/master/example/example.ipynb
 
 def get_unique_locations(df: pd.DataFrame):
     """Get unique locations with an alternate base"""
     locations = df["location"].drop_duplicates().dropna().to_list()
     return locations
+
 
 def _get_pids(df: pd.DataFrame) -> set:
     pids = set(df["pid"].drop_duplicates().to_list())
@@ -38,7 +38,9 @@ def plot_boxplot(df: pd.DataFrame, y_variable: str, location: int) -> None:
     # get the reference base
     reference = set(subset_df["reference"].to_list())
     # there should only be one
-    assert len(reference) == 1, f"There is more than one reference base at position f{location}!"
+    assert (
+        len(reference) == 1
+    ), f"There is more than one reference base at position f{location}!"
     reference = reference.pop()
 
     # the subset will only contain individuals with an alternate base
@@ -53,53 +55,57 @@ def plot_boxplot(df: pd.DataFrame, y_variable: str, location: int) -> None:
     data = data.assign(
         location=[location] * len(data),
         reference=[reference] * len(data),
-        alternate=["reference"] * len(data)
+        alternate=["reference"] * len(data),
     )
     # concatenate to subset df now
     concat_df = pd.concat([subset_df, data], axis=0).sort_values(by=["pid"])
-    
 
     fig = make_subplots(
-        rows=2, cols=1,
-        vertical_spacing=0.03,
-        specs=[
-            [{"type": "table"}],
-            [{"type": "xy"}]],
-            row_heights=[1, 1.5]
-        )
+        rows=1,
+        cols=2,
+        specs=[[{"type": "table"}, {"type": "xy"}]],
+        column_widths=[0.5, 1.5],
+        horizontal_spacing=0.05,
+    )
 
     bases = ["reference"] + [x for x in BASES if x != reference]
 
     stats_df = {"base": [], "p-value": []}
 
     for x in bases:
-        fig.add_trace(go.Box(
-            y=concat_df[concat_df['alternate'] == x][y_variable],
-            name=x,
-            boxpoints='outliers'
-        ), row=2, col=1)
+        fig.add_trace(
+            go.Box(
+                y=concat_df[concat_df["alternate"] == x][y_variable],
+                name=x,
+                boxpoints="outliers",
+            ),
+            row=1,
+            col=2,
+        )
 
         # do a t-test with each base with reference
         if x != "reference":
             pvalue = stats.ttest_ind(
                 concat_df[concat_df["alternate"] == "reference"][y_variable],
-                concat_df[concat_df["alternate"] == x][y_variable]
+                concat_df[concat_df["alternate"] == x][y_variable],
             )[1]
 
             stats_df["base"].append(x)
             stats_df["p-value"].append(round(pvalue, 3))
-    
+
+    fig.update_yaxes(title_text=y_variable, row=1, col=2)
+    fig.update_xaxes(title_text="base", row=1, col=2)
+
     fig.add_trace(
         go.Table(
-            header=dict(values=["Base", "P-value"]),
-            cells=dict(values=[stats_df[k] for k in stats_df.keys()])
+            header=dict(values=["base", "p-value"]),
+            cells=dict(values=[stats_df[k] for k in stats_df.keys()]),
         ),
-        row=1, col=1
+        row=1,
+        col=1,
     )
 
-    fig.update_layout(
-        height=600
-    )
+    fig.update_layout(height=600, margin=dict(l=100, r=20, t=20, b=20))
 
     return fig
 
@@ -109,45 +115,55 @@ def main():
     assembled_df = assemble(options.input_stem)
 
     locations = get_unique_locations(assembled_df)
+    locations = sorted(locations)
 
     app = Dash(__name__)
 
-    app.layout = html.Div([
-        html.Div([
-            html.Div([
-                dcc.Dropdown(
-                    options=sorted(x for x in assembled_df["location"].dropna().unique()),
-                    # label='Team',
-                    id='variant-select',
-                    value=locations[0],  # arbitrarily show the first position
-                ),
-            ], style={'width': '50%'})
-        ]),
-
-        dcc.Graph(id='variant'),
-    ])
-
-    @app.callback(
-        Output("variant", "figure"),
-        Input("variant-select", "value")
+    app.layout = html.Div(
+        [
+            html.Div(
+                [
+                    html.Div(
+                        [
+                            "Position:",
+                            dcc.Dropdown(
+                                options=sorted(x for x in assembled_df["location"].dropna().unique()),
+                                id="variant-select",
+                                value=locations[0],  # arbitrarily show the first position
+                            ),
+                        ],
+                        style={"width": "10%", "margin-left": "100px", "margin-bottom": "0px", "margin-top": "50px", "display": "inline-block"},
+                    ),
+                    html.Div(
+                        [dcc.Graph(id="variant")],
+                        style={"width": "75%"}
+                    )
+                ]
+            ),
+        ]
     )
+
+    @app.callback(Output("variant", "figure"), Input("variant-select", "value"))
     def update_graph(location):
-        # dff = assembled_df[assembled_df["location"] == location]
         fig = plot_boxplot(assembled_df, "weight", location)
         return fig
-    
-    app.run_server(debug=True)
 
+    app.run_server(debug=True)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     # nothing here for now
     parser.add_argument(
-        "--input_stem", type=str, default=None, help="Path/file stem of synthesized data", required=True
+        "--input_stem",
+        type=str,
+        default=None,
+        help="Path/file stem of synthesized data",
+        required=True,
     )
     options = parser.parse_args()
     return options
+
 
 if __name__ == "__main__":
     main()
