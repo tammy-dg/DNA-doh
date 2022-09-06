ead57aaab221d80bc2bbcdc95786dbb3686c345d
kevqyzhu <kevqyzhu@gmail.com>
2022-09-06 00:09:29 -0400

    added structure for plotting functionality

--- dnadoh/analyze.py
+++ dnadoh/analyze.py
@@ -13,6 +13,7 @@ from plotly.subplots import make_subplots
 
 from dash import Dash, dcc, html, Input, Output
 from scipy import stats
+from dnadoh_plot.plot_util import plot_boxplot
 
 from assemble import assemble
 
@@ -26,94 +27,6 @@ def get_unique_locations(df: pd.DataFrame) -> list:
 
 def _get_pids(df: pd.DataFrame) -> set:
     return set(df["pid"].drop_duplicates().to_list())
-
-
-def plot_boxplot(df: pd.DataFrame, y_variable: str, location: int) -> None:
-    """
-    Creates boxplots of continuous variable 'y' on the y-axis
-    for each reference base.
-    """
-    # subset dataframe to the location
-    subset_df = df[df["location"] == location]
-
-    # get the reference base
-    reference = set(subset_df["reference"].to_list())
-
-    # there should only be one
-    assert (
-        len(reference) == 1
-    ), f"There is more than one reference base at position f{location}!"
-    reference = reference.pop()
-
-    # the subset will only contain individuals with an alternate base
-    # to include individuals with the reference base,
-    # we need to figure out who those are and add them back in
-    all_pids = _get_pids(df)
-    subset_pids = _get_pids(subset_df)
-    leftover_pids = all_pids.difference(subset_pids)
-
-    # build df of info for pids that have the reference base at this position
-    # drop duplicates due to joining of phenotypic + variant data
-    data = df[["pid", "age", "gsex", "weight"]].drop_duplicates()
-    data = data[data["pid"].isin(leftover_pids)]
-    data = data.assign(
-        location=[location] * len(data),
-        reference=[reference] * len(data),
-        alternate=["reference"] * len(data),
-    )
-
-    # concatenate to the df with the pids with the reference base to the 
-    # originally subset df so that we get one df with all pids
-    concat_df = pd.concat([subset_df, data], axis=0).sort_values(by=["pid"])
-
-    fig = make_subplots(
-        rows=1,
-        cols=2,
-        specs=[[{"type": "table"}, {"type": "xy"}]],
-        column_widths=[0.5, 1.5],
-        horizontal_spacing=0.05,
-    )
-
-    bases = ["reference"] + [x for x in BASES if x != reference]
-
-    stats_df = {"base": [], "p-value": []}
-
-    for x in bases:
-        fig.add_trace(
-            go.Box(
-                y=concat_df[concat_df["alternate"] == x][y_variable],
-                name=x,
-                boxpoints="outliers",
-            ),
-            row=1,
-            col=2,
-        )
-
-        # do a t-test with each base with reference
-        if x != "reference":
-            pvalue = stats.ttest_ind(
-                concat_df[concat_df["alternate"] == "reference"][y_variable],
-                concat_df[concat_df["alternate"] == x][y_variable],
-            )[1]
-
-            stats_df["base"].append(x)
-            stats_df["p-value"].append(round(pvalue, 3))
-
-    fig.update_yaxes(title_text=y_variable, row=1, col=2)
-    fig.update_xaxes(title_text="base", row=1, col=2)
-
-    fig.add_trace(
-        go.Table(
-            header=dict(values=["base", "p-value"]),
-            cells=dict(values=[stats_df[k] for k in stats_df.keys()]),
-        ),
-        row=1,
-        col=1,
-    )
-
-    fig.update_layout(height=600, margin=dict(l=100, r=20, t=20, b=20))
-
-    return fig
 
 
 def main():
